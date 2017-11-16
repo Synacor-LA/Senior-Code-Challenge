3e473748938b7b4d57132dedabb45d31a469e11f
Wade Armstrong <wade.armstrong@synacor.com>
2017-11-15 18:06:47 -0800

    actually works

--- functions/index.js
+++ functions/index.js
@@ -8,20 +8,14 @@ const googleMapsClient = require('@google/maps').createClient({
 
 function getGoogleGeocode(city, state) {
 	return googleMapsClient.geocode({
-		address: `${city}, ${state}`
+		address: `${city},+${state}`
 	}).asPromise();
 }
 
 exports.getLatLong = functions.https.onRequest((req, res) => {
-	const state = req.state;
-	const city = req.city;
+
+	const state = req.query.state;
+	const city = req.query.city;
 
 	return getGoogleGeocode(city, state).then(geocode => res.send(geocode.json.results));
 })
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
