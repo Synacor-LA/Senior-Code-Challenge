79fe96321a73c130a177d89e7b7ef6413bac5043
Wade Armstrong <wade.armstrong@synacor.com>
2017-11-15 17:36:52 -0800

    does things

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,24 @@
 const functions = require('firebase-functions');
 
+const googleMapsClient = require('@google/maps').createClient({
+	key: 'AIzaSyDII_KvbCZSapZTDZ99t4eZgxgZds2yJl4',
+	Promise: Promise
+});
+
+
+function getGoogleGeocode(city, state) {
+	return googleMapsClient.geocode({
+		address: `${city}, ${state}`
+	}).asPromise();
+}
+
+exports.getLatLong = functions.https.onRequest((req, res) => {
+	const state = req.state;
+	const city = req.city;
+
+	return getGoogleGeocode(city, state).then(geocode => res.send(geocode.json.results));
+})
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
